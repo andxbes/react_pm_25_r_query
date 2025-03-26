de9e9809905470a25445ecabb713cc109b892b2b
andxbes <andxbes@gmail.com>
2025-03-26 22:51:54 +0200

    414

--- src/components/Events/FindEventSection.jsx
+++ src/components/Events/FindEventSection.jsx
@@ -9,9 +9,10 @@ export default function FindEventSection() {
   const searchElement = useRef();
   const [searchTerm, setSearchTerm] = useState();
 
-  const { data, isPending, isError, error } = useQuery({
+  const { data, isLoading, isError, error } = useQuery({
     queryKey: ['events', { search: searchTerm }],
-    queryFn: () => fetchEvents({ 'search': searchTerm })
+    queryFn: ({ signal }) => fetchEvents({ searchTerm: { 'search': searchTerm }, signal }),
+    enabled: searchTerm !== undefined
   });
 
   function handleSubmit(event) {
@@ -21,7 +22,7 @@ export default function FindEventSection() {
 
   let content = <p>Please enter a search term and to find events.</p>;
 
-  if (isPending) {
+  if (isLoading) {
     content = <LoadingIndicator />;
   }
 
@@ -31,7 +32,7 @@ export default function FindEventSection() {
 
   if (data) {
     content = <ul className='events-list'>
-      {data.map(event => <li key={event}><EventItem event={event} /></li>)}
+      {data.map((event, index) => <li key={'search-' + index}><EventItem event={event} /></li>)}
     </ul>;
   }
 
--- src/utils/http.js
+++ src/utils/http.js
@@ -1,13 +1,15 @@
-export async function fetchEvents(searchTerm) {
+export async function fetchEvents({ signal, searchTerm }) {
     let url = new URL('http://localhost:3000/events');
 
     for (let key in searchTerm) {
-        url.searchParams.set(key, searchTerm[key]);
+        if (searchTerm[key]) {
+            url.searchParams.set(key, searchTerm[key]);
+        }
     }
 
-    console.info(url.toString());
+    // console.info(url.toString());
 
-    const response = await fetch(url.toString());
+    const response = await fetch(url.toString(), { signal });
 
     if (!response.ok) {
         const error = new Error('An error occurred while fetching the events');
