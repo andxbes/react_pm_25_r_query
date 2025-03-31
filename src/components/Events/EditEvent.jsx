ee991663deebc52269a17f49ed71f601ba4ba5f2
andxbes <andxbes@gmail.com>
2025-03-31 22:17:27 +0300

    423

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -2,8 +2,8 @@ import { Link, useNavigate, useParams } from 'react-router-dom';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
-import { useQuery } from '@tanstack/react-query';
-import { fetchEvent } from '../../utils/http.js';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { fetchEvent, updateEvent } from '../../utils/http.js';
 import LoadingIndicator from '../UI/LoadingIndicator.jsx';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 
@@ -16,7 +16,15 @@ export default function EditEvent() {
     queryFn: ({ signal }) => { fetchEvent({ signal, id }) }
   });
 
-  function handleSubmit(formData) { }
+  const { mutate } = useMutation({
+    mutationFn: updateEvent,
+    onSuccess: () => { }
+  });
+
+  function handleSubmit(formData) {
+    mutate({ id, event: formData });
+    navigate('../');
+  }
 
   function handleClose() {
     navigate('../');
